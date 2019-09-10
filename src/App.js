4bada168938130c70b9dd7cdc966804af37c7345
ofega <fegaeze@gmail.com>
2019-09-10 11:53:31 +0100

    rendered display component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from "react";
 import "./App.css";
+import Display from './components/DisplayComponents/Display'
 import Operators from './components/ButtonComponents/OperatorButtons/Operators';
 import Numbers from './components/ButtonComponents/NumberButtons/Numbers';
 import Specials from './components/ButtonComponents/SpecialButtons/Specials'
-// STEP 4 - import the button and display components
-// Don't forget to import any extra css/scss files you build into the correct component
-
-// Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
 
 function App() {
@@ -20,6 +17,7 @@ function App() {
     <div className="container">
       <Logo />
       <div className="App">
+        <Display />
         <Operators />
         <Numbers />
         <Specials />
